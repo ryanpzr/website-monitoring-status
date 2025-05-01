7b51e51085891c9ece3397b1d96be425cddbd81b
ryanpzr <ryanpereiralimads@gmail.com>
2025-05-01 03:38:00 -0300

    Create monitor that verify the websites status and logical

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,12 @@ import (
 	"log"
 	"net/http"
 	"website-monitoring/internal/router"
+	"website-monitoring/internal/service"
 )
 
 func main() {
 	r := router.SetupRouter()
-
+	go service.VerifacateWebStatus()
 	log.Println("Starting server on :8080...")
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatalf("Could not start server: %v\n", err)
