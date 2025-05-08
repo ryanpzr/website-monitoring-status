273d87911528785c905321ab298e5dcd2ffecd39
ryanpzr <ryanpereiralimads@gmail.com>
2025-05-08 00:57:48 -0300

    Enviroment variables configured

--- configs/dbconfig/db.go
+++ configs/dbconfig/db.go
@@ -2,12 +2,28 @@ package dbconfig
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func OpenConn() (*sql.DB, error) {
-	db, err := sql.Open("postgres", "host=localhost port=5432 user=postgres password=Oscaveiras1! dbname=monitoring_db sslmode=disable")
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatal("Erro ao carregar o arquivo .env")
+	}
+
+	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_SSLMODE"),
+	))
 
 	if err != nil {
 		panic(err)
